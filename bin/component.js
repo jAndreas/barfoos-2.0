841c8ab4018da5fea0fa8169d38c1aecddb99120
Andreas Göbel <andreas.goebel@judgemy.org>
2017-09-18 18:23:27 +0200

    added helper binaries to create basic strucures

---
+++ bin/component.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+"use strict";
+
+const	path	= require( 'path' ),
+		fs		= require( 'fs' ),
+		cmd		= require( 'commander' ),
+		folders	= [ 'fonts', 'images', 'markup', 'js', 'style' ];
+
+const mkdirSync = path => {
+	try {
+		fs.mkdirSync( path );
+	} catch (err) {
+		if (err.code !== 'EEXIST') throw err
+	}
+}
+
+cmd
+	.arguments( '<moduleName>' )
+	.action(name => {
+		mkdirSync( `${__dirname}/modules` );
+		mkdirSync( `${__dirname}/modules/${ name }` );
+
+		for( let folder of folders ) {
+			mkdirSync( `${__dirname}/modules/${ name }/${ folder }` );
+		}
+	}).parse( process.argv );
