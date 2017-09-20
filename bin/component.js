248cc787f65852b44e379557a9a88e662cc4fbe5
Andreas Göbel <andreas.goebel@typeofnan.com>
2017-09-20 02:53:11 +0200

    - added blueprint version of js, html and css for newly created BarFoos components via script

--- bin/component.js
+++ bin/component.js
@@ -1,10 +1,78 @@
 #!/usr/bin/env node
 'use strict';
 
+const jsblueprint =
+`'use strict';
+
+import { Component } from 'barfoos2.0/core.js';
+import { moduleLocations } from 'barfoos2.0/defs.js';
+import { extend } from 'barfoos2.0/toolkit.js';
+
+import { htmlx } from '../markup/main.htmlx';
+import { style } from '../style/main.scss';
+
+/*****************************************************************************************************
+ *  "description here"
+ *****************************************************************************************************/
+class MyModule extends Components {
+	constructor( input = { }, options = { } ) {
+		extend( options ).with({
+			location:		moduleLocations.center,
+			tmpl:			htmlx({
+
+			})
+		}).and( input );
+
+		super( options );
+
+		this.runtimeDependencies.push(
+			this.fire( 'waitforHLSSupport.appEvents' )
+		);
+
+		return this.init();
+	}
+
+	async init() {
+		// any component related declarations, bindings, listeners etc. which can get executed immediately, before we wait for possible dependencies
+
+		await super.init();
+
+		// any component related stuff which should wait on dependencies resolve before executing (waitForDOM at least or additional async data)
+
+		return this;
+	}
+}
+/****************************************** MyModule End ******************************************/
+
+async function start() {
+	[ style ].forEach( style => style.use() );
+
+	const instance = await new MyModule();
+}
+
+export { start };`;
+
+const cssviewport =
+`$defaultTextColor:rgba(220,220,220,0.95);
+$defaultHoverColor:rgba(30, 40, 70, 0.8);
+$iPhone6Portrait:415px;
+$iPhone6Landscape:736px;
+$iPadPortrait:768px;`;
+
+const cssmain = `@import 'viewportDefinitions';`;
+
+const htmlmain = `<div id="MyModule"></div>`;
+
 const	path	= require( 'path' ),
 		fs		= require( 'fs' ),
 		cmd		= require( 'commander' ),
-		folders	= [ 'fonts', 'images', 'markup', 'js', 'style' ];
+		folders = {
+			fonts:	[ ],
+			images:	[ ],
+			markup: [ { 'main.htmlx': htmlmain } ],
+			js:		[ { 'main.js': jsblueprint } ],
+			style:	[ { '_viewportDefinitions.scss': cssviewport, 'main.scss': cssmain } ]
+		};
 
 const mkdirSync = path => {
 	try {
@@ -20,7 +88,13 @@ cmd
 		mkdirSync( `${__dirname}/modules` );
 		mkdirSync( `${__dirname}/modules/${ name }` );
 
-		for( let folder of folders ) {
+		for( let [ folder, files ] of Object.entries( folders ) ) {
 			mkdirSync( `${__dirname}/modules/${ name }/${ folder }` );
+
+			for( let file of files ) {
+				Object.keys( file ).forEach( filename => {
+					fs.writeFileSync( `${__dirname}/modules/${ name }/${ folder }/${ filename }`, file[ filename ].replace( /MyModule/, name) );
+				});
+			}
 		}
 	}).parse( process.argv );
